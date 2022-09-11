d62e90cd3686986491e787dc75cf059c317d8902
KartikPC <87184927+KartikPC@users.noreply.github.com>
2022-09-11 07:42:27 -0700

    update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+icon = document.querySelector('.icon')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+navlista = document.querySelector('.navlisth a')
+
+icon.addEventListener('click',()=>{
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('v-class-resp');
+    
+    
+    
+})
+
+
+  
+
+  
+
+
+
+
+
